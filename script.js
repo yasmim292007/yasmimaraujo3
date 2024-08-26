3bcdec0ee10c6ad06ab56212f506077ae30fb664
camilapessoa <camilabentespessoa@gmail.com>
2024-08-26 20:32:42 -0300

    update index script

--- script.js
+++ script.js
@@ -36,8 +36,3 @@ document.addEventListener('DOMContentLoaded', function(){
  
  
  })
- 
- ScrollReveal().reveal('#inicio', { delay: 500 });
- ScrollReveal().reveal('#tropicalia', { delay: 500 });
- ScrollReveal().reveal('#galeria', { delay: 500 });
- ScrollReveal().reveal('#contato', { delay: 500 });
